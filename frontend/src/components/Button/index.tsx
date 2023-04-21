e40cf8cdfed71b6c0805885c200bc4e801614263
hyoingwoon <cpobo@hanmail.net>
2023-04-21 15:44:00 +0900

    Feat: Add Storybook

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, HTMLProps } from 'react';
+import { ButtonLayout, ButtonLayoutProps } from './styles';
+
+export interface ButtonProps extends HTMLProps<HTMLButtonElement>, ButtonLayoutProps {}
+
+const Button: FunctionComponent<ButtonProps> = ({ children, buttonType, isFull }) => {
+    return (
+        <ButtonLayout type="button" buttonType={buttonType} isFull={isFull}>
+            {children}
+        </ButtonLayout>
+    );
+};
+
+export default Button;
