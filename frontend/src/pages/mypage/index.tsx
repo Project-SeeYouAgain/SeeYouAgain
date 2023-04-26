8d801853c9c323a7c501a362ff57e5dee3e4fb88
OctoHun <tjd_gns12@naver.com>
2023-04-26 16:56:46 +0900

    [FE] feat: 마이페이지

--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import Header from '../../components/header/Header';
+import Profile from '../../components/Card/Profile';
+import { MenuData1, MenuData2 } from './menuData';
+import Link from 'next/link';
 
 function MyPage() {
     return (
         <div>
             <Header title="마이페이지" />
-            <div></div>
+            <main className="mt-[3rem]">
+                <Profile />
+                <div className="mt-[2.3rem]">
+                    <div className="flex flex-col">
+                        <span className="font-bold text-[20px]">나의 거래</span>
+                        {MenuData1.map((item, index) => (
+                            <Link key={index} href={item.url} className={`text-[16px] ${index === 0 ? 'mt-[1rem]' : 'mt-[0.5rem]'}`}>
+                                {item.title}
+                            </Link>
+                        ))}
+                    </div>
+                    <div className="flex flex-col mt-[2.3rem]">
+                        <span className="font-bold text-[20px]">기타</span>
+                        {MenuData2.map((item, index) => (
+                            <Link key={index} href={item.url} className={`text-[16px] ${index === 0 ? 'mt-[1rem]' : 'mt-[0.5rem]'}`}>
+                                {item.title}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
