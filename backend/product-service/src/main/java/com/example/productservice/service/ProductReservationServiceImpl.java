fb3ddf7c16b72c8de8d7ad0b32e5bf7499871d73
나웅기 <nwk2222@naver.com>
2023-04-27 11:31:44 +0900

    Feature/product crud

---
+++ backend/product-service/src/main/java/com/example/productservice/service/ProductReservationServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.productservice.service;
+
+import com.example.productservice.client.UserServiceClient;
+import com.example.productservice.dto.request.ProductReservationRequestDto;
+import com.example.productservice.dto.response.ReservationResponseDto;
+import com.example.productservice.entity.Product;
+import com.example.productservice.entity.Reservation;
+import com.example.productservice.exception.ApiException;
+import com.example.productservice.exception.ExceptionEnum;
+import com.example.productservice.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductReservationServiceImpl implements ProductReservationService {
+
+    private final ProductRepository productRepository;
+
+    private final ProductImgRepository productImgRepository;
+
+    private final ProductTagRepository productTagRepository;
+
+    private final ReservationRepository reservationRepository;
+
+    private final ReviewRepository reviewRepository;
+
+    private final UserServiceClient userServiceClient;
+
+    @Override
+    @Transactional
+    public void createReservation(Long userId, Long productId, ProductReservationRequestDto requestDto) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ApiException(ExceptionEnum.PRODUCT_NOT_EXIST_EXCEPTION));
+
+        Reservation reservation = Reservation.of(userId, product, requestDto);
+        reservationRepository.save(reservation);
+    }
+
+    @Override
+    public ReservationResponseDto getReservationList(Long userId) {
+        return null;
+    }
+
+
+}
