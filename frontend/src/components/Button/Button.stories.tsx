95f58312fdaa24e97860db18e94e64ffd27e5d25
OctoHun <tjd_gns12@naver.com>
2023-04-27 16:05:44 +0900

    [FE] feat: 버튼 컴포넌트 생성

--- frontend/src/components/Button/Button.stories.tsx
+++ frontend/src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import Button, { ButtonProps } from './index';
+import Button, { ButtonProps } from '.';
 
 export default {
     title: 'Atoms/Button',
@@ -36,4 +36,41 @@ MiddleModal.args = {
     buttonType: 'MiddleModal',
     color: 'blue',
     children: '대여일정',
+};
+
+export const MiddleModalRed = Template.bind({});
+
+MiddleModalRed.args = {
+    buttonType: 'MiddleModalRed',
+    color: 'blue',
+    children: '대여일정',
+};
+
+export const MiddleModalOrange = Template.bind({});
+
+MiddleModalOrange.args = {
+    buttonType: 'MiddleModalOrange',
+    color: 'blue',
+    children: '대여일정',
+};
+
+export const Cate1 = Template.bind({});
+
+Cate1.args = {
+    buttonType: 'Cate1',
+    children: '도서',
+};
+
+export const Cate2 = Template.bind({});
+
+Cate2.args = {
+    buttonType: 'Cate2',
+    children: '여성잡화',
+};
+
+export const Cate3 = Template.bind({});
+
+Cate3.args = {
+    buttonType: 'Cate3',
+    children: '가구/인테리어',
 };
---
+++ frontend/src/components/Button/color.ts
@@ -0,0 +1,14 @@
+export const backgroundColor = {
+    red: 'bg-[#FF6262]',
+    black: 'bg-black',
+    blue: 'bg-[#5669FF]',
+    white: 'bg-white',
+    lightgray: 'bg-[#F2F2F2]',
+    orange: 'bg-[#FF8655]',
+    darkgray: 'bg-[#8E8E93]',
+};
+
+export const txtColor = {
+    white: 'text-white',
+    black: 'text-black',
+};
