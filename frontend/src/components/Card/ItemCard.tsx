9c50c36d4f5743eeeffed5b638049c9eddf943fa
OctoHun <tjd_gns12@naver.com>
2023-04-28 15:43:27 +0900

    [FE] feat: 아이템 카드 컴포넌트 완성

--- frontend/src/components/Card/ItemCard.tsx
+++ frontend/src/components/Card/ItemCard.tsx
@@ -1,26 +1,27 @@
 import React, { FunctionComponent, HTMLProps } from 'react';
 import Image from 'next/image';
-import TempImage from '../../assets/icons/phone.png';
 import Button from '../Button';
 import { AiFillStar } from 'react-icons/ai';
 
-function ItemCard() {
+function ItemCard(data: object) {
     return (
         <div className="w-[100%]">
-            <span className="bg-black rounded-full text-white px-[0.8rem]">2023.03.30 ~ 2023.04.20</span>
+            <span className="bg-black rounded-full text-white px-[0.8rem]">
+                {data.startDate} ~ {data.endDate}
+            </span>
             <div className="flex">
                 <div className="w-[25%] relative mr-[0.8rem]">
-                    <Image src={TempImage} alt="임시 사진" className="aspect-square" layout="fill" objectFit="cover" />
+                    <Image src={data.productImg[0]} alt="임시 사진" className="aspect-square" layout="fill" objectFit="cover" />
                 </div>
                 <div className="flex flex-col">
-                    <span className="font-bold w-[100%] truncate">닌텐도 스위치/라이트 대여 가능</span>
-                    <span className="text-[#8E8E93]">장덕동</span>
+                    <span className="font-bold w-[100%] truncate">{data.title}</span>
+                    <span className="text-[#8E8E93]">{data.location}</span>
                     <div className="flex">
-                        <span className="font-bold">4,400</span>
+                        <span className="font-bold">{data.price}</span>
                         <span className="text-[#8E8E93] mr-[2rem]">원 /일</span>
                         <span className="flex items-center text-darkgrey">
                             <AiFillStar className="mr-[0.2rem]" color="darkgrey" />
-                            4.8
+                            {data.score}
                         </span>
                     </div>
                     <div className="flex items-center">
