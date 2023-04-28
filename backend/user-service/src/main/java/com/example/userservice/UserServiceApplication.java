b7f73bb8d66f26b9aad07014324cc6983cb1ab72
keeeeeey <sseioul@naver.com>
2023-04-28 22:45:17 +0900

    Feat: 카카오 로그인 구현

--- backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,19 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
 	}
 
 }
