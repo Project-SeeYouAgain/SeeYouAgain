f6b1fcd8ab2a7a9958c157573f29e39e3e33665d
choihyoingwoon <cpobo@hanmail.net>
2023-05-02 23:28:20 +0900

    Feat: Add 프로필 수정

--- frontend/src/components/Button/SquareLg.tsx
+++ frontend/src/components/Button/SquareLg.tsx
@@ -6,15 +6,16 @@ type ButtonProps = {
     textColor: keyof typeof txtColor;
     innerValue: React.ReactNode;
     className?: string;
+    divClass?: string;
     onClick?: () => void;
 };
 
-function RoundSm({ check = true, bgColor, textColor, innerValue, className = '', onClick }: ButtonProps) {
+function RoundSm({ check = true, bgColor, textColor, innerValue, divClass = '', className = '', onClick }: ButtonProps) {
     return (
-        <div>
+        <div className={`${divClass}`}>
             {check && (
                 <button
-                    className={`flex justify-center items-center rounded-[0.62rem] w-[100%] h-[3.06rem] font-bold text-xl ${backgroundColor[bgColor]} ${txtColor[textColor]} ${className}`}
+                    className={`flex justify-center items-center rounded-[0.62rem] w-full h-[3.06rem] font-bold text-xl ${backgroundColor[bgColor]} ${txtColor[textColor]} ${className}`}
                     type="button"
                     onClick={onClick}
                 >
--- frontend/src/components/Container/components/Header/index.tsx
+++ frontend/src/components/Container/components/Header/index.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 const Header: FunctionComponent<HeaderProps> = ({ title }) => {
     return (
-        <div className="relative">
+        <div className="relative p-4">
             <AiOutlineLeft color="#5669FF" size="24" />
             <span className="absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] font-bold text-[20px] text-[#5669FF]">{title}</span>
         </div>
