5dd6960301a5d9f7e1ba3c969473f1f025625698
hyoingwoon <cpobo@hanmail.net>
2023-05-02 11:07:39 +0900

    Feat: Add contract

--- frontend/src/components/Button/SquareLg.tsx
+++ frontend/src/components/Button/SquareLg.tsx
@@ -1,14 +1,37 @@
 import { backgroundColor, txtColor } from './color';
 
-function RoundSm({ bgColor, textColor, innerValue, className, onClick }: ButtonProps) {
+type ButtonProps = {
+    check?: boolean;
+    bgColor: keyof typeof backgroundColor;
+    textColor: keyof typeof txtColor;
+    innerValue: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
+};
+
+function RoundSm({ check = true, bgColor, textColor, innerValue, className = '', onClick }: ButtonProps) {
     return (
-        <button
-            className={` flex justify-center items-center rounded-[0.62rem] w-[100%] h-[3.06rem] font-bold text-[1.5rem] ${backgroundColor[bgColor]} ${txtColor[textColor]} ${className}`}
-            type="button"
-            onClick={onClick}
-        >
-            {innerValue}
-        </button>
+        <div>
+            {check && (
+                <button
+                    className={`flex justify-center items-center rounded-[0.62rem] w-[100%] h-[3.06rem] font-bold text-xl ${backgroundColor[bgColor]} ${txtColor[textColor]} ${className}`}
+                    type="button"
+                    onClick={onClick}
+                >
+                    {innerValue}
+                </button>
+            )}
+            {!check && (
+                <button
+                    className={`flex justify-center items-center rounded-[0.62rem] w-[100%] h-[3.06rem] font-bold text-xl bg-gray-300 ${txtColor[textColor]} ${className}`}
+                    type="button"
+                    onClick={onClick}
+                    disabled
+                >
+                    {innerValue}
+                </button>
+            )}
+        </div>
     );
 }
 
