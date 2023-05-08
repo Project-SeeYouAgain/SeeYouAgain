ccf9a85110d25dba5367c0945085a269b43c8146
최형운 <cpobo@hanmail.net>
2023-05-08 17:44:39 +0900

    Feature/page

--- frontend/src/pages/chat/[identifier]/pick-location.tsx
+++ frontend/src/pages/chat/[identifier]/pick-location.tsx
@@ -4,6 +4,7 @@ import KakaoMap from '@/components/Location/SetKakaoMap';
 import pin from '@/images/location-pin.png';
 import Image from 'next/image';
 import styles from './userLocation.module.scss';
+import Swal from 'sweetalert2';
 
 const UserLocation: React.FC = () => {
     const [isMobile, setIsMobile] = useState<boolean | null>(null);
@@ -14,8 +15,80 @@ const UserLocation: React.FC = () => {
     };
     const [myCheck, setMyCheck] = useState(true);
     const clickPosition = () => {
-        myCheck ? setMyCheck(false) : setMyCheck(true);
+        console.log(lat, lng, score);
+        if (score >= 7.5) {
+            Swal.fire({
+                title: '안전지수 3단계',
+                text: '주변에 CCTV,가로등,경찰서이 \n 모두 있습니다.',
+                icon: 'success', // 성공, 에러, 경고 등의 아이콘을 선택할 수 있습니다.
+                confirmButtonText: '확인', // 버튼 텍스트를 지정할 수 있습니다.
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: '취소',
+            }).then(result => {
+                if (result.isConfirmed) {
+                    // 확인 버튼 클릭 시 처리할 로직
+                } else if (result.isDenied) {
+                    // 취소 버튼 클릭 시 처리할 로직
+                }
+            });
+        } else if (score >= 5) {
+            Swal.fire({
+                title: '안전지수 2단계',
+                text: '주변에 CCTV,가로등, 경찰서이 \n여러개 있습니다.\n3개다 있진 않을수도 있습니다.',
+                icon: 'success', // 성공, 에러, 경고 등의 아이콘을 선택할 수 있습니다.
+                confirmButtonText: '확인', // 버튼 텍스트를 지정할 수 있습니다.
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: '취소',
+            }).then(result => {
+                if (result.isConfirmed) {
+                    // 확인 버튼 클릭 시 처리할 로직
+                } else if (result.isDenied) {
+                    // 취소 버튼 클릭 시 처리할 로직
+                }
+            });
+        } else if (score >= 2.5) {
+            Swal.fire({
+                title: '안전지수 1단계',
+                text: '주변에 CCTV 또는 가로등이 \n1개 있습니다. 경찰서는 없습니다.',
+                icon: 'success', // 성공, 에러, 경고 등의 아이콘을 선택할 수 있습니다.
+                confirmButtonText: '확인', // 버튼 텍스트를 지정할 수 있습니다.
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: '취소',
+            }).then(result => {
+                if (result.isConfirmed) {
+                    // 확인 버튼 클릭 시 처리할 로직
+                } else if (result.isDenied) {
+                    // 취소 버튼 클릭 시 처리할 로직
+                }
+            });
+        } else {
+            Swal.fire({
+                title: '안전지대 밖입니다.',
+                text: '주변에 경찰서,CCTV,가로등이 없어요\n계속진행하실건가요?',
+                icon: 'warning', // 성공, 에러, 경고 등의 아이콘을 선택할 수 있습니다.
+                confirmButtonText: '확인', // 버튼 텍스트를 지정할 수 있습니다.
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: '취소',
+            }).then(result => {
+                if (result.isConfirmed) {
+                    // 확인 버튼 클릭 시 처리할 로직
+                } else if (result.isDenied) {
+                    // 취소 버튼 클릭 시 처리할 로직
+                }
+            });
+        }
     };
+    const [lng, setLng] = useState<number>(0);
+    const [lat, setLat] = useState<number>(0);
+    const [score, setScore] = useState<number>(0);
 
     const message = '이 페이지는 모바일 기기에서 최적화되어 있습니다. 모바일로 접속해주세요.';
 
@@ -56,10 +129,19 @@ const UserLocation: React.FC = () => {
                         </div>
                     )}
                     <div id="map" className="w-full h-[85vh] relative">
-                        <KakaoMap lat={userLocation.lat} lng={userLocation.lng} />
-                        <div className="absolute bottom-10 w-full z-10" onClick={clickPosition}>
-                            {myCheck && <p className="w-2/3 h-12 rounded-xl text-center text-white text-xl m-auto bg-blue pt-2.5">장소 확정</p>}
-                            {!myCheck && <p className="w-2/3 h-12 rounded-xl text-center text-white text-xl m-auto bg-gray-400 pt-2.5">확정 완료</p>}
+                        <KakaoMap
+                            lat={userLocation.lat}
+                            lng={userLocation.lng}
+                            onCenter={(lat, lng, score) => {
+                                setLat(lat);
+                                setLng(lng);
+                                setScore(score);
+                            }}
+                        />
+                        <div className="absolute bottom-10 z-10 w-full">
+                            <button className="w-2/3 h-12 m-auto block rounded-xl text-center text-white text-xl  bg-blue" onClick={clickPosition}>
+                                장소 확정
+                            </button>
                         </div>
                     </div>
                 </>
