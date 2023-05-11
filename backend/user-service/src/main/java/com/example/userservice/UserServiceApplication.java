f39959ca5dcbbb2fc6ba982b3721608ab2ffe510
keeeeeey <sseioul@naver.com>
2023-05-11 14:43:56 +0900

    Fix: user-service

--- backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,8 +1,10 @@
 package com.example.userservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,8 +12,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@EnableFeignClients
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
