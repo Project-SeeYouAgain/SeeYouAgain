439aa1737d6595a2983acfde93c68757701ecea4
김예은 <suntofu0228@naver.com>
2023-05-11 16:34:59 +0900

    [FE] fix : 다크모드 배경색 수정

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
                     <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=09c93f3059a9f8639cf78e21b3ae44a6&libraries=services"></script>
                     <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
                 </Head>
-                <body className="bg-white">
+                <body className="bg-white ">
                     <Main />
                     <NextScript />
                 </body>
