b44222481558267338665ed0290bb8baf98c52f0
김예은 <suntofu0228@naver.com>
2023-05-14 16:50:48 +0900

    글작성 거래장소 선택 및 세이프존 선택 수정

--- frontend/src/apis/axiosinstance.tsx
+++ frontend/src/apis/axiosinstance.tsx
@@ -3,9 +3,9 @@ import axios, { AxiosInstance } from 'axios';
 export const interceptors = (instance: AxiosInstance, token: string | null) => {
     instance.interceptors.request.use(
         config => {
-            // const tokens = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3IiwiaWF0IjoxNjg0MDQwMzcxLCJleHAiOjE2ODQxMjY3NzF9.xJF-82BVN4L4fkFnPuJ_MExRx6Z0pLD3kZy1N-KzNrU';
-
+            // const tokens = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwiaWF0IjoxNjg0MDQ0NDk2LCJleHAiOjE2ODQxMzA4OTZ9.YsH3WsbVkbCngu-AIZHhPPdKSnoOk4YX936_7xitkFg';
             config.headers.Authorization = `Bearer ${token}`;
+
             return config;
         },
         error => Promise.reject(error.response),
