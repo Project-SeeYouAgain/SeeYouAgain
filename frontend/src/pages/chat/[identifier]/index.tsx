afc1455efb2a0b36eca2208b1c8172797e8df002
keeeeeey <sseioul@naver.com>
2023-05-17 09:48:14 +0900

    Fix: 채팅 서비스 무한 스크롤 수정

--- frontend/src/pages/chat/[identifier]/index.tsx
+++ frontend/src/pages/chat/[identifier]/index.tsx
@@ -246,7 +246,7 @@ function Channel() {
 
     useEffect(() => {
         scrollToBottom();
-    }, [chatList]);
+    }, [chatList[0]]);
 
     const goToBook = () => [router.push(`/chat/${identifier}/book/${channelInfo?.productId}`)];
     const goToUserLocation = () => {
@@ -260,7 +260,7 @@ function Channel() {
     };
 
     return (
-        <div className="relative pt-48">
+        <div className="relative pt-48 h-screen">
             <div className="fixed inset-x-0 top-0 bg-white z-50">
                 {channelInfo && (
                     <div className="p-5 text-center border-b border-gray flex justify-between items-center">
@@ -299,7 +299,7 @@ function Channel() {
                 </div>
             </div>
 
-            <div className="chat-list mx-5 pb-16 h-fit" style={{ overflow: 'auto' }}>
+            <div className="chat-list mx-5 pb-16 h-screen" style={{ overflow: 'auto' }}>
                 <InfiniteScroll initialLoad={false} loadMore={getMessage} hasMore={hasMore} isReverse={true} useWindow={false} threshold={50}>
                     {chatList
                         .slice()
