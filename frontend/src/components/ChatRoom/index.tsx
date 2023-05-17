1a96f403af5fddcf2f2fbc9d36bc2b63c9bbf60c
choihyoingwoon <cpobo@hanmail.net>
2023-05-17 22:41:50 +0900

    Fix: book and write

--- frontend/src/components/ChatRoom/index.tsx
+++ frontend/src/components/ChatRoom/index.tsx
@@ -46,27 +46,25 @@ function timeAgo(date: string): string {
 function ChatRoom({ profileImg, nickname, location, latestMessageDate, latestMessage, productImg, identifier, notReadMessageSize }: ChatRoomProps) {
     return (
         <Link href={`/chat/${identifier}`} className="border-b-1 flex mb-5 items-center" role="button">
-            <div className="w-9/12 flex items-center">
-                <div className="w-3/12 me-2 relative" style={{ width: 45, height: 45 }}>
-                    <Image src={profileImg ? profileImg : default_user} alt="프로필 이미지" className="rounded-full object-cover" fill />
-                </div>
-                <div className="w-9/12">
-                    <div className="items-end">
-                        <div className="flex justify-between items-center">
-                            <p className="font-medium me-2">{nickname}</p>
-                            <p className="text-gray-500 me-1 font-light text-xs">{location}</p>
-                            <p className="text-gray-500 font-light text-xs">
-                                <span className="flex items-center justify-center">
-                                    <BsDot className="mr-[2px]" />
-                                    {timeAgo(latestMessageDate)}
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                    <p className="truncate">{latestMessage}</p>
-                </div>
+            <div className="w-[50px] relative" style={{ width: 50, height: 50 }}>
+                <Image src={profileImg ? profileImg : default_user} alt="프로필 이미지" className="rounded-full object-cover" fill />
             </div>
-            <div className="w-3/12 flex justify-end items-center">
+            <div className="grow items-center px-2">
+                <div className="flex justify-between items-center">
+                    <p className="font-medium">{nickname}</p>
+                    <span className="flex">
+                        <p className="text-gray-500 font-light text-xs">{location}</p>
+                        <p className="text-gray-500 font-light text-xs">
+                            <span className="flex items-center justify-center">
+                                <BsDot className="mr-[1px]" />
+                                {timeAgo(latestMessageDate)}
+                            </span>
+                        </p>
+                    </span>
+                </div>
+                <p className="truncate">{latestMessage}</p>
+            </div>
+            <div className="w-[50px] flex justify-end items-center">
                 {notReadMessageSize > 0 && <p className="text-white text-center text-sm rounded-full bg-blue aspect-square w-5 h-5 me-2">{notReadMessageSize}</p>}
                 <div className="relative" style={{ width: 50, height: 50 }}>
                     <Image src={productImg} alt="물품 이미지" className="rounded-md object-cover" fill />
